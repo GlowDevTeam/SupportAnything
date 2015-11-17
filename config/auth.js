ad731b674a4f2f5eb113b70d829906d0a9ad577a
sanxzkid <f.sanxz@gmail.com>
2015-11-17 18:31:15 -0200

    Change callback url

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '1688283328124624', // your App ID
 		'clientSecret' 	: '0236cf62072f42ff052f150d29a54431', // your App Secret
-		'callbackURL' 	: 'http://localhost:8085/auth/facebook/callback'
+		'callbackURL' 	: 'https://supportanything.herokuapp.com/auth/facebook/callback'
 	}
 
 };
