7950914a07395f5dc3b497cd47d209a5d228beff
sanxzkid <f.sanxz@gmail.com>
2015-11-19 04:23:05 -0200

    Update css loading

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -6,19 +6,28 @@ $.noConflict();
       var myimage = $(this);
       var imagemask = myimage.attr('src');
 
+      var allimages = $('.support-row img');
+      if (allimages.data("executing")) return;
+
       $.ajax({
         url: 'run',
         type: 'POST',
         data: JSON.stringify({ mask: imagemask }) ,
         contentType: 'application/json; charset=utf-8',
-        // loading image if take long
-        /*beforeSend: function() {
+        beforeSend: function() {
+          allimages
+            .data("executing", true)
+            .attr('style', 'cursor:wait');
+
           $('.result img').attr({
             'src': '/Images/load.gif',
             'style': 'opacity: 0.8'
           });
-        },*/
+        },
         success: function(result){
+          allimages
+            .removeData("executing")
+            .attr('style', 'cursor:pointer');
           $('.result img').attr({
             'src': '/Images/' + result + '.jpg?t='+ Date.now(),
             'style': ''
@@ -27,6 +36,10 @@ $.noConflict();
       });
 
     });
+
+    function applyMask () {
+
+    }
 
   });
 })(jQuery);
