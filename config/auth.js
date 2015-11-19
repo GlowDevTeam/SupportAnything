319b25d1998756b6458a658816531564ca146e0b
sanxzkid <f.sanxz@gmail.com>
2015-11-19 03:33:08 -0200

    auth update

--- config/auth.js
+++ config/auth.js
@@ -4,9 +4,10 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '1688283328124624', // your App ID
 		'clientSecret' 	: '0236cf62072f42ff052f150d29a54431', // your App Secret
-		'callbackURL' 	: 'https://supportanything.herokuapp.com/auth/facebook/callback'
+		'callbackURL' 	: 'http://supportanything.azurewebsites.net/auth/facebook/callback'
 		//'callbackURL' 	: 'http://localhost:5000/auth/facebook/callback',
 		//'alternative'		: 'https://supportanything.herokuapp.com/auth/facebook/callback'
+		//'alternative'		: 'http://supportanything.azurewebsites.net/auth/facebook/callback'
 	}
 
 };
